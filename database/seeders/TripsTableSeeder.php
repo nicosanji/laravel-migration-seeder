bfa51d8e8a1ae94ef074b6f1a6e063b20e5a29b6
nicosanji <n.sangiorgio92@gmail.com>
2022-01-12 09:17:29 +0100

    Creati model e seeder (la tabella non si popola)

--- database/seeders/TripsTableSeeder.php
+++ database/seeders/TripsTableSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use Faker\Generator as Faker;
+use App\Models\Trip;
+
 class TripsTableSeeder extends Seeder
 {
     /**
@@ -11,8 +14,15 @@ class TripsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 100; $i++) {
+            $newTrips = new Trip();
+            $newTrips->country = $faker->state();
+            $newTrips->airline = $faker->company();
+            $newTrips->description = $faker->paragraph();
+            $newTrips->votes = $faker->randomDigitNotNull();
+            $newTrips->save();
+        }
     }
 }
